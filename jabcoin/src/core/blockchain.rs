656af53ba3582f7064a79d6ee34e18f88d9e1c45
jabra11 <jabra11.gpg@gmail.com>
2022-05-09 01:13:34 +0200

    sketch out verification & block appendation logic

--- jabcoin/src/core/blockchain.rs
+++ jabcoin/src/core/blockchain.rs
@@ -1,9 +1,12 @@
-use crate::core::block::Block;
+use std::collections::HashMap;
 
 pub struct Blockchain
 {
     length: u64,
-    head: Option<Block>,
+
+    // holds the hash of the latest block
+    head: Option<Vec<u8>>,
+    blocks: HashMap<Vec<u8>, Block>,
 }
 
 impl Blockchain
@@ -13,6 +16,7 @@ impl Blockchain
         Blockchain {
             length: 0,
             head: None,
+            blocks: HashMap::new(),
         }
     }
 
@@ -20,7 +24,7 @@ impl Blockchain
     {
         match &self.head
         {
-            Some(h) => Some(h),
+            Some(h) => Some(&self.blocks[h]),
             None => None,
         }
     }
@@ -30,21 +34,127 @@ impl Blockchain
         self.length
     }
 
-    pub fn verify(&self, _block: &Block) -> bool
+    fn verify(&self, block: &Block) -> Result<(), String>
     {
-        // what constitutes a valid block?
-        todo!();
+        // naive consensus, needs to start with 000
+        if !block.hash_str().starts_with("000")
+        {
+            return Err(format!("hash is invalid: {}", block.hash_str()));
+        }
+
+        for i in block.transactions()
+        {
+            if let Some(sig) = &i.signature()
+            {
+                if let Err(e) = i.input().get_addr().verify_data(&i.hash_ignore_sig(), sig)
+                {
+                    return Err(format!(
+                        "failed to verify signature for transaction: {} with error {}",
+                        i.hash_str(),
+                        e
+                    ));
+                }
+            }
+            else
+            {
+                return Err(format!(
+                    "signature missing for transaction: {}",
+                    i.hash_str()
+                ));
+            }
+
+            let out_sum = i
+                .output()
+                .transactors()
+                .iter()
+                .fold(0, |acc, trxactor| acc + trxactor.get_value());
+
+            if out_sum > i.input().get_value()
+            {
+                return Err(format!(
+                    "insufficient input value in transaction: {}:\n {} < {}",
+                    i.hash_str(),
+                    i.input().get_value(),
+                    out_sum
+                ));
+            }
+        }
+
+        Ok(())
     }
 
-    pub fn append_block(&mut self, block: Block) -> Result<(), (&'static str, Block)>
+    pub fn append_block(&mut self, mut block: Block) -> Result<(), (String, Block)>
     {
-        if Blockchain::verify(self, &block)
+        if let Err(e) = Blockchain::verify(self, &block)
         {
-            todo!();
+            Err((
+                format!("block verification failed with error: {}", e),
+                block,
+            ))
         }
         else
         {
-            Err(("block verification failed!", block))
+            if let Some(last) = self.head()
+            {
+                *block.hash_prev() = last.hash();
+            }
+            else
+            {
+                *block.hash_prev() = Vec::new();
+            }
+
+            let h = block.hash();
+            self.head = Some(h.clone());
+            self.blocks.insert(h, block);
+            self.length += 1;
+
+            Ok(())
         }
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+    use crate::core::address::Address;
+    use crate::core::transaction::{Input, Output, Transaction};
+    use rsa::RsaPrivateKey;
+
+    #[test]
+    fn verify_block()
+    {
+        let mut rng = rand::thread_rng();
+        let rsa = RsaPrivateKey::new(&mut rng, 1024).unwrap();
+        let initiator = Address::with_key(rsa.to_public_key());
+
+        let recipient = Address::new();
+        let inp = Input::new(initiator, 100);
+        let outp = Output::with_addrs(vec![(recipient, 10)]).unwrap();
+
+        let mut trx = Transaction::new(inp.clone(), outp.clone());
+
+        let sig = rsa
+            .sign(
+                rsa::PaddingScheme::new_pkcs1v15_sign(None),
+                &trx.hash_ignore_sig(),
+            )
+            .unwrap();
+
+        trx.set_signature(sig);
+
+        let mut blk = Block::new();
+        blk.add_transaction(trx);
+        loop
+        {
+            if blk.hash_str().starts_with("000")
+            {
+                break;
+            }
+            blk.update_nounce()
+        }
+
+        let mut blkchain = Blockchain::new();
+        blkchain.append_block(blk).unwrap();
     }
 }
