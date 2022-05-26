75748bf3ab29a59ed456ff540dba0d49c0fe4533
jabra11 <jabra11.gpg@gmail.com>
2022-05-26 23:30:36 +0200

    only store id,ipaddr pairs for connected nodes

--- jabcoin/src/network/node.rs
+++ jabcoin/src/network/node.rs
@@ -1,20 +1,51 @@
+use serde::{Deserialize, Serialize};
 use std::net::Ipv4Addr;
 
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 pub struct Node
 {
     id: u64,
     address: Ipv4Addr,
-    connected_nodes: Vec<Node>,
+    connected_nodes: Vec<(u64, Ipv4Addr)>,
 }
 
 impl Node
 {
-    pub fn new() -> Node
+    pub fn new(id: u64, address: Ipv4Addr) -> Node
     {
         Node {
-            id: 0,
-            address: Ipv4Addr::new(0, 0, 0, 0),
+            id,
+            address,
             connected_nodes: vec![],
         }
+    }
+
+    pub fn with_nodes(id: u64, address: Ipv4Addr, nodes: Vec<(u64, Ipv4Addr)>) -> Node
+    {
+        Node {
+            id,
+            address,
+            connected_nodes: nodes,
+        }
+    }
+
+    pub fn connect_node(&mut self, node: Node)
+    {
+        self.connected_nodes.push((node.id, node.address));
+    }
+
+    pub fn id(&self) -> u64
+    {
+        self.id
+    }
+
+    pub fn address(&self) -> &Ipv4Addr
+    {
+        &self.address
+    }
+
+    pub fn connected_nodes(&self) -> &Vec<(u64, Ipv4Addr)>
+    {
+        &self.connected_nodes
     }
 }
