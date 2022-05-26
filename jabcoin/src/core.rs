5d5374cc337c4cfd48f99858cc3e76e6bdc7c6aa
jabra11 <jabra11.gpg@gmail.com>
2022-05-26 23:29:57 +0200

    reexport core types

--- jabcoin/src/core.rs
+++ jabcoin/src/core.rs
@@ -3,3 +3,8 @@ pub mod block;
 pub mod blockchain;
 pub mod crypto;
 pub mod transaction;
+
+pub use address::Address;
+pub use block::Block;
+pub use blockchain::Blockchain;
+pub use transaction::{Input, Output, Transaction, Transactor};
--- jabcoin/src/network.rs
+++ jabcoin/src/network.rs
@@ -1,2 +1,4 @@
 pub mod node;
 pub mod protocol;
+
+pub use node::Node;
