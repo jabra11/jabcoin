dad3f5e722c2cca219728aae5b757c7f42dfcdfd
jabra11 <jabra11.gpg@gmail.com>
2022-06-08 01:56:05 +0200

    some refactoring

--- jabcoin/tests/common/mod.rs
+++ jabcoin/tests/common/mod.rs
@@ -1,19 +1,45 @@
-use jabcoin::core::crypto::Sha256Hash;
+use jabcoin::core::crypto::{generate_random_rsa_pair, Sha256Hash};
 use jabcoin::core::{Address, Block, Input, Output, Transaction};
 use jabcoin::network::Node;
 
-pub fn setup_mock_block() -> Block
+use rand::{thread_rng, Rng};
+
+pub fn setup_mock_block(count_transactions: u64, count_distinct_transactors: u64) -> Block
 {
     let mut blk = Block::new();
 
-    let initiator = Address::generate_random();
-    let recipient = Address::generate_random();
+    let mut transactors = Vec::with_capacity(count_distinct_transactors as usize);
 
-    for i in 1..100
+    for i in 0..count_distinct_transactors
     {
-        let inp = Input::new(initiator.clone(), i);
-        let outp = Output::with_addrs(vec![(recipient.clone(), i)]).unwrap();
-        let trx = Transaction::with_signature(inp, outp, vec![123, 12, 31, 23, 123]);
+        let rsa_pair = generate_random_rsa_pair();
+        let pubaddr = Address::with_key(rsa_pair.to_public_key());
+        transactors.push((rsa_pair, pubaddr));
+    }
+
+    for _ in 0..count_transactions
+    {
+        let mut r = thread_rng();
+        let initiator: usize = r.gen_range(0..count_distinct_transactors) as usize;
+
+        // let's allow addresses to transact with themselves
+        let recipient: usize = r.gen_range(0..count_distinct_transactors) as usize;
+
+        let input = Input::new(transactors[initiator].1.clone(), r.gen_range(1..=10000));
+        let output =
+            Output::with_addrs(vec![(transactors[recipient].1.clone(), input.get_value())])
+                .unwrap();
+
+        let mut trx = Transaction::new(input, output);
+
+        let padding = rsa::PaddingScheme::new_pkcs1v15_sign(None);
+
+        trx.set_signature(
+            transactors[initiator]
+                .0
+                .sign(padding, &trx.hash_ignore_sig())
+                .unwrap(),
+        );
         blk.add_transaction(trx);
     }
 
