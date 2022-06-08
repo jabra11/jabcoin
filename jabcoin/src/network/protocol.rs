e2433c783100e607b2b0c3487bc1d8d0df7093f8
jabra11 <jabra11.gpg@gmail.com>
2022-06-08 18:08:40 +0200

    some additions to the network protocol

--- jabcoin/src/network/protocol.rs
+++ jabcoin/src/network/protocol.rs
@@ -3,6 +3,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub enum Header
 {
+    // broadcast a transaction
+    BroadcastTransaction,
+
     // broadcast a block
     BroadcastBlock,
 
@@ -20,6 +23,12 @@ pub enum Header
 
     // Deregister from the p2p-network
     Deregister,
+
+    // generic OK
+    OK,
+
+    // generic ERR
+    ERR,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -27,6 +36,38 @@ pub struct Message
 {
     pub header: Header,
 
-    // key:value pairs, not efficient but simple
-    pub data: Vec<(String, String)>,
+    // json format, not efficient but simple
+    pub body: String,
+}
+
+impl Message
+{
+    pub fn new() -> Message
+    {
+        Message::with_data(Header::OK, "")
+    }
+
+    pub fn with_data(header: Header, body: &str) -> Message
+    {
+        Message {
+            header,
+            body: String::from(body),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    #[test]
+    fn serialize()
+    {
+        todo!()
+    }
+
+    #[test]
+    fn deserialize()
+    {
+        todo!();
+    }
 }
