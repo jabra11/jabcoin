7fe4e9f702b9db3ef5b0c2f21dd8f8a018a5b784
jabra11 <jabra11.gpg@gmail.com>
2022-06-26 20:54:49 +0200

    add peer module

---
+++ jabnode/src/network/peer.rs
@@ -0,0 +1,71 @@
+use serde::{Deserialize, Serialize};
+use std::net::Ipv4Addr;
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+pub enum PeerType
+{
+    FullNode,
+    LightNode,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
+pub struct Peer
+{
+    id: u64,
+    ptype: PeerType,
+    address: Ipv4Addr,
+    connected_nodes: Vec<(u64, Ipv4Addr)>,
+}
+
+impl Peer
+{
+    pub fn new(id: u64, ptype: PeerType, address: Ipv4Addr) -> Peer
+    {
+        Peer {
+            id,
+            ptype,
+            address,
+            connected_nodes: vec![],
+        }
+    }
+
+    pub fn with_nodes(
+        id: u64,
+        ptype: PeerType,
+        address: Ipv4Addr,
+        nodes: Vec<(u64, Ipv4Addr)>,
+    ) -> Peer
+    {
+        Peer {
+            id,
+            ptype,
+            address,
+            connected_nodes: nodes,
+        }
+    }
+
+    pub fn connect_node(&mut self, node: Peer)
+    {
+        self.connected_nodes.push((node.id, node.address));
+    }
+
+    pub fn id(&self) -> u64
+    {
+        self.id
+    }
+
+    pub fn ptype(&self) -> PeerType
+    {
+        self.ptype
+    }
+
+    pub fn address(&self) -> &Ipv4Addr
+    {
+        &self.address
+    }
+
+    pub fn connected_nodes(&self) -> &Vec<(u64, Ipv4Addr)>
+    {
+        &self.connected_nodes
+    }
+}
